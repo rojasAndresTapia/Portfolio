612620391e0f8e72d60706d3e433cc274e223b41
Andrés Rojas <eurekacloud2019@gmail.com>
2022-05-12 12:41:18 +0200

    first commit

---
+++ src/api/api-model.ts
@@ -0,0 +1,12 @@
+export interface DeliveryService {
+  // available_pickup_dates: {};
+  // available_delivery_dates: {};
+  // carrier_name: string;
+  // name: string;
+  // currency: string;
+  // category: string;
+  // transit_time: string;
+  // transit_hours: string;
+  // base_price: string;
+  // service_info: [{}];
+}
