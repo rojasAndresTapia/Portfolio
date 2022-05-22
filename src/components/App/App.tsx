b9f1fb83b04e15f8e12fbfb86f30faa8f88ee11e
Andrés Rojas <eurekacloud2019@gmail.com>
2022-05-22 20:22:51 +0200

    Change scenes to Scenes

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { ContactPage } from '../../scenes/Contact/ContactPage';
-import { DesignPage } from '../../scenes/Design/DesignPage';
-import { Home } from '../../scenes/Home/Home';
-import { VideoPage } from '../../scenes/Video/VideoPage';
-import { WordpressPage } from '../../scenes/Wordpress/WordpressPage';
+import { ContactPage } from '../../Scenes/Contact/ContactPage';
+import { DesignPage } from '../../Scenes/Design/DesignPage';
+import { Home } from '../../Scenes/Home/Home';
+import { VideoPage } from '../../Scenes/Video/VideoPage';
+import { WordpressPage } from '../../Scenes/Wordpress/WordpressPage';
 import { NavBar } from '../NavBar/NavBar';
 import { Footer } from '../Footer/Footer';
 import { Categories } from '../Categories/Categories';
-import { GithubPage } from '../../scenes/Github/GihubPage';
+import { GithubPage } from '../../Scenes/Github/GihubPage';
 
 export const App: React.FC = () => {
   return (
